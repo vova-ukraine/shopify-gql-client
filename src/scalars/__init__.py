d336bc40d55625f7e9a6e9c7dfe06d6a25e6540c
Volodymyr Vorobei <vova.ukraine@gmail.com>
2025-01-26 18:31:43 +0200

    Add empty shcema dirs

--- src/scalars/__init__.py
+++
@@ -1,4 +0,0 @@
-from .int import Int
-from .string import String
-
-__all__ = ("Int",  "String")
--- src/scalars/boolean.py
+++
@@ -1,9 +0,0 @@
-from .scalar import Scalar
-from .exceptions import ValidationError
-
-
-class Boolean(Scalar):
-    def validate(cls, value: bool) -> bool:
-        if not isinstance(value, bool):
-            raise ValidationError(f"Invalid Boolean value: {value}")
-        return value
