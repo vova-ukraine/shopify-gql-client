963a6e4971b2c1540baea6ef0d180233426b6809
Volodymyr Vorobei <vova.ukraine@gmail.com>
2025-01-26 18:25:12 +0200

    Remove  schema/scalars/* from Git index

--- src/schema/scalars/boolean.py
+++
@@ -1,9 +0,0 @@
-from base_types import ScalarType
-from exceptions import ValidationError
-
-class Boolean(ScalarType, bool):
-    @classmethod
-    def validate(cls, value: bool) -> bool:
-        if not isinstance(value, bool):
-            raise ValidationError(f"Invalid Boolean value: {value}")
-        return value
--- src/schema/scalars/id.py
+++
@@ -1,9 +0,0 @@
-from base_types import ScalarType
-from exceptions import ValidationError
-
-class ID(ScalarType, str):
-    @classmethod
-    def validate(cls, value: str) -> str:
-        if not isinstance(value, str):
-            raise ValidationError(f"Invalid ID value: {value}")
-        return value
